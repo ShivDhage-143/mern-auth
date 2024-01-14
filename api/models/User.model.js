830efc3bb6ce47be850d6cb7677b0af4c0fd382f
Shiv Dhage <shivdhage4@gmail.com>
2024-01-14 18:57:13 +0530

    create user model

---
+++ api/models/User.model.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({  //it is like authentication means only this type of data are acceptable.
+    username:{                               //if you know yup or formik validation so it is smilar kind of that.
+        required: true,
+        unique:true,
+    },
+    email:{
+        type:String,
+        required: true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required: true,
+    }
+},{timestamps: true});
+
+const User = mongoose.model('User',userSchema);
+
+export default User;
